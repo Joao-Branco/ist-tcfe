8386616712e60ef3ebcfb59d03edd41b296aa48a
Joao-Branco <you@example.com>
2021-05-08 18:40:15 +0100

    final do lab 3

--- t3/mat2/oct2.m
+++ t3/mat2/oct2.m
@@ -17,7 +17,7 @@ tOFF = 1/w * atan(-C*R*w);
 k = 1;
 vOnexp = A*sin(w*tOFF)*exp(-(t-tOFF)/R/C);
 
-figure
+%%figure
 for i=1:length(t)   %%%%%retificador de onda completa
   if (vS(i) > 0)
     vOhr(i) = vS(i);
@@ -26,8 +26,8 @@ for i=1:length(t)   %%%%%retificador de onda completa
   endif
 endfor
 
-plot(t*1000, vOhr)
-hold
+%%%plot(t*1000, vOhr)
+%%%hold
 
 for i=1:length(t)
   if t(i) < tOFF
@@ -52,16 +52,37 @@ endif
 endfor
 vOmin
 
-plot(t*1000, vO)
+%%%%plot(t*1000, vO)
+%%grid on
+%%%title("Output voltage v_o(t)")
+%%%%xlabel ("t[ms]")
+%%%%legend("full-wave rectified","envelope")
+%%%%print ("venvlope.eps", "-depsc");
+
+
+ripplee=max(vO)-vOmin;
+ripplef=ripplee*459.828/(459.828+100*10^3);
+
+
+printf("ripple_TAB\n")
+Min = vOmin* 12 /169.9608
+Ripple = ripplef
+Max = max(vO)* 12 /169.9608
+printf("ripple_END\n");
+
+
+vOf = vO * 12 /169.9608;
+
+figure
+plot(t*1000, vOf)
+axis([1e-3*1000, 200e-3*1000, 8, 13])
 grid on
 title("Output voltage v_o(t)")
 xlabel ("t[ms]")
-legend("full-wave rectified","envelope")
-print ("venvlope.eps", "-depsc");
+legend("Voltage Regulator","envelope")
+print ("Vregulator.eps", "-depsc");
 
 
-ripple=max(vO)-vOmin;
-ripplef=ripple*459.828/(459.828+100*10^3);
 
 
 
